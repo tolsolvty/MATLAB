e66b6cd41027e0800ac469a156dccb6630200d67
Maxim Smolskiy <mithridatus@mail.ru>
2021-08-23 23:29:19 +0300

    Ускорена реализация подпрограммы calcfg (#10)

--- src/tolsolvty.m
+++ src/tolsolvty.m
@@ -197,88 +197,32 @@ function [f,g,tt] = calcfg(x)
 %   функция, которая вычисляет значение f максимизируемого распознающего 
 %   функционала и его суперградиент g;  кроме того, она выдаёт вектор tt 
 %   из значений образующих функционала в данной точке аргумента 
-% 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%   предварительное размещение рабочих массивов 
+    %   для быстрого вычисления образующих распознающего функционала
+    %   используются сокращённые формулы умножения интервальной матрицы
+    %   на точечный вектор, через середину и радиус 
+    Axc = Ac * x;
+    Axr = Ar * abs(x);
+    infs = bc - (Axc + Axr);
+    sups = bc - (Axc - Axr);
+    tt = weight .* (br - max(abs(infs), abs(sups)));
   
-infs = zeros(m,1); 
-sups = zeros(m,1); 
-tt = zeros(m,1); 
-dl = zeros(n,1); 
-ds = zeros(n,1); 
-dd = zeros(n,m); 
+    %   сборка значения всего распознающего функционала 
+    [f, mc] = min(tt);
   
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %   вычисляем значение распознающего функционала и матрицу dd, 
-    %   составленную из суперградиентов его образующих 
-    
-    for i = 1:m 
-        los = 0.5*(infb(i)+supb(i));
-        sus = los; 
-        for j = 1:n
-            if x(j) >= 0  
-                los = los - supA(i,j)*x(j); 
-                sus = sus - infA(i,j)*x(j); 
-            else 
-                los = los - infA(i,j)*x(j); 
-                sus = sus - supA(i,j)*x(j); 
-            end 
-        end 
-        %   нижний и верхний концы интервала, который получается  
-        %   под модулем в выражении для i-ой образующей 
-        infs(i) = los; 
-        sups(i) = sus; 
+    %   вычисление суперградиента той образующей распознающего функционала, 
+    %   на которой достигается предыдущий минимум 
+    isxnonnegative = x >= 0; 
+    isxnegative = x < 0; 
+    if -infs(mc) <= sups(mc) 
+        ds(isxnonnegative,1) = infA(mc,isxnonnegative); 
+        ds(isxnegative,1) = supA(mc,isxnegative); 
+        g = weight(mc) * ds; 
+    else 
+        dl(isxnonnegative,1) = supA(mc,isxnonnegative); 
+        dl(isxnegative,1) = infA(mc,isxnegative); 
+        g = -weight(mc) * dl; 
     end 
-    
-    %   вычисление значения i-ой образующей распознающего 
-    %   функционала и её суперградиента 
-    for i = 1:m 
-        alos = abs(infs(i));  asus = abs(sups(i)); 
-        %   вычисление суперградиента dl нижнего конца
-        for j = 1:n 
-            dm = infA(i,j);
-            dp = supA(i,j);
-            if x(j) < 0
-                dl(j) = dm;
-            else 
-                dl(j) = dp;
-            end
-        end
-        %   вычисление суперградиента ds верхнего конца 
-        for j = 1:n 
-            dm = supA(i,j);
-            dp = infA(i,j);
-            if x(j) < 0 
-                ds(j) = dm;
-            else
-                ds(j) = dp;
-            end
-        end
-        %   сборка полного суперградиента i-ой образующей
-        if alos ~= asus
-            if alos < asus 
-                mags = asus;  dd(:,i) = weight(i)*ds; 
-            else 
-                mags = alos;  dd(:,i) = -weight(i)*dl; 
-            end 
-        else
-            mags = alos; 
-            if sups(i) > 0 
-                dd(:,i) = weight(i)*ds; 
-            else 
-                dd(:,i) = -weight(i)*dl; 
-            end
-        end 
-        %   нахождение и запоминание значения i-ой образующей 
-        tt(i) = weight(i)*(0.5*(supb(i) - infb(i)) - mags);
-    end  
-   
-    %   выбираем минимальную по значению образующую
-    %   и конструируем общий суперградиент 
-    [f,mc] = min(tt);
-    g = dd(:,mc);
-  
 end 
   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -289,7 +233,9 @@ end
 %   иначе берём начальным приближением нулевой вектор 
 % 
 Ac = 0.5*(infA + supA);
+Ar = 0.5*(supA - infA);
 bc = 0.5*(infb + supb);
+br = 0.5*(supb - infb);
 sv = svd(Ac);
 minsv = min(sv);
 maxsv = max(sv);
--- test/performance/test_calcfg.m
+++ test/performance/test_calcfg.m
@@ -1,11 +1,16 @@
 addpath(fullfile('..', '..', 'src'));
 
 load(fullfile('data', '1000.mat'));
+
+Ac = (infA + supA) / 2;
+Ar = (supA - infA) / 2;
+
+bc = (infb + supb) / 2;
+br = (supb - infb) / 2;
+
 weight = ones([m, 1]);
 
-mid_A = (infA + supA) / 2;
-mid_b = (infb + supb) / 2;
-x = mid_A \ mid_b;
+x = Ac \ bc;
 
 %%
-calcfg(x, m, n, infA, supA, infb, supb, weight);
+calcfg(x, infA, supA, Ac, Ar, bc, br, weight);
