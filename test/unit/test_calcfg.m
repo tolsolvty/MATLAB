d5354a940de7d46c1359e15c61fae91dafede21d
MaximSmolskiy <mithridatus@mail.ru>
2021-08-23 23:19:55 +0300

    Обновлены тесты

--- test/unit/test_calcfg.m
+++ test/unit/test_calcfg.m
@@ -10,12 +10,19 @@ listing = dir('data');
 for file = listing(~[listing.isdir])'
     
     load(fullfile(file.folder, file.name));
+    
+    Ac = (infA + supA) / 2;
+    Ar = (supA - infA) / 2;
+    
+    bc = (infb + supb) / 2;
+    br = (supb - infb) / 2;
+    
     weight = ones([m, 1]);
     
     for i = 1 : N
         
         x = 2 * (rand([n, 1]) - 0.5) * 10 ^ randi([0, ceil(log10(max(1, norm(gt_argmax)))) + 1]);
-        [~, g, ~] = calcfg(x, m, n, infA, supA, infb, supb, weight);
+        [~, g, ~] = calcfg(x, infA, supA, Ac, Ar, bc, br, weight);
         
         numerical_g = zeros([n, 1]);
         for j = 1 : n
@@ -24,10 +31,10 @@ for file = listing(~[listing.isdir])'
             step = h * double((1 : n)' == j);
             
             x_plus_step = x + step;
-            f_x_plus_step = calcfg(x_plus_step, m, n, infA, supA, infb, supb, weight);
+            f_x_plus_step = calcfg(x_plus_step, infA, supA, Ac, Ar, bc, br, weight);
             
             x_minus_step = x - step;
-            f_x_minus_step = calcfg(x_minus_step, m, n, infA, supA, infb, supb, weight);
+            f_x_minus_step = calcfg(x_minus_step, infA, supA, Ac, Ar, bc, br, weight);
             
             numerical_g(j) = (f_x_plus_step - f_x_minus_step) / (2 * h);
         end
