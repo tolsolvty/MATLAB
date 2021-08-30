463f2c87b4fd8290a5f9347685d8931728889331
Maxim Smolskiy <mithridatus@mail.ru>
2021-08-30 23:05:43 +0300

    Ускорена реализация подпрограммы calcfg (#12)

--- src/tolsolvty.m
+++ src/tolsolvty.m
@@ -215,10 +215,12 @@ function [f,g,tt] = calcfg(x)
     isxnonnegative = x >= 0; 
     isxnegative = x < 0; 
     if -infs(mc) <= sups(mc) 
+        ds = zeros(n,1); 
         ds(isxnonnegative,1) = infA(mc,isxnonnegative); 
         ds(isxnegative,1) = supA(mc,isxnegative); 
         g = weight(mc) * ds; 
     else 
+        dl = zeros(n,1); 
         dl(isxnonnegative,1) = supA(mc,isxnonnegative); 
         dl(isxnegative,1) = infA(mc,isxnegative); 
         g = -weight(mc) * dl; 
